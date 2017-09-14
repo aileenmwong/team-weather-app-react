8293a24a469e1a29972a2b8575339ebe82a21882
Aileen Wong <aileen.m.wong@gmail.com>
2017-09-14 16:19:41 -0400

    add weather form and weather data page

--- team-weather-app/src/components/Weather.js
+++ team-weather-app/src/components/Weather.js
@@ -1,1 +1,17 @@
-Weather.js
+import React, { Component } from 'react';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div id="weatherPageWrapper">
+        <div id="resultWeather">
+          <div id="cityName">City Name{}</div>
+          <div id="currentTemp">Current Temp{}</div>
+          <div id="description">Description{}</div>
+          <div id="minTemp">Min Temp{}</div>
+          <div id="maxTemp">Max Temp{}</div>
+        </div>
+      </div>
+    )
+  }
+}
